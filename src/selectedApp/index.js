c38c003334f8d6ed5229cac57b23b77d362357ce
huanglvming <huanglvming@ecqun.com>
2018-03-30 09:54:31 +0800

    创建rect模板

---
+++ src/selectedApp/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class SelectedApp extends Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
